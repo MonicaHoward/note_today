faafd38c7622c7aa2ab464b85516bf0226aea805
Monica Howard <jay.m.howard@gmail.com>
2018-07-19 20:03:53 -0500

    Added Bootstrap, sprockets and simple form gems

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -7,9 +7,16 @@ class NotesController < ApplicationController
   end
 
   def new
+    @note = Note.new
   end
 
   def create
+    @note = Note.new(note_params)
+    if @note.save
+      redirect_to @note
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -26,7 +33,8 @@ class NotesController < ApplicationController
   def find_note
   end
 
-  def find_params
+  def note_params
+    params.require(:note).permit(:title, :content)
   end
 
 
