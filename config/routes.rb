b23a25802ed503b01921b6dafc6aa642d9aacb23
Monica Howard <jay.m.howard@gmail.com>
2018-08-09 11:24:24 -0500

    Bug fix for notes for authenticated users in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  authenticated :user do
+    root "notes#index", as: "authenticated_root"
+  end
+
   resources :notes
   get 'pages/index'
 
 
   root 'pages#index'
 
-  authenticated :user do
-    root "notes#index", as: "authenticated_root"
-  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
