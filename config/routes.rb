cdae2b4657fc3e97ef106b5edb256f3a9294757b
Monica Howard <jay.m.howard@gmail.com>
2018-08-21 00:42:42 -0500

    Fixed authentication view bug and application index view

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,7 @@ Rails.application.routes.draw do
   authenticated :user do
     root "notes#index", as: "authenticated_root"
   end
+
+  root "pages#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
